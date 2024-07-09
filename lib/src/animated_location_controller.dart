2c0562f713f01d0101f64d7da5fcdf724fa43159
Robbendebiene <robbendebiene@mailbox.org>
2024-07-09 15:12:49 +0200

    Move location and sensor logic to controller

--- lib/src/animated_location_controller.dart
+++ lib/src/animated_location_controller.dart
@@ -1,7 +1,16 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
+import 'package:flutter_sensors/flutter_sensors.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
 /// Can be used to retrieve the underlying values (real and interpolated) that drive the indicators.
+///
+/// Notifies whenever `location`, `accuracy`, `orientation` or `isActive` changes.
+///
+/// The uninterpolated values can be accessed via `rawOrientation`, `rawAccuracy`, `rawOrientation` and listened to via `addRawListener()`.
 
 abstract class AnimatedLocationController implements ChangeNotifier {
   // Factory constructor redirects to underlying implementation's constructor.
@@ -16,15 +25,158 @@ abstract class AnimatedLocationController implements ChangeNotifier {
   /// Orientation may be null if the device doesn't provide the respective sensors.
 
   double? get orientation;
+
+
+  /// Location may be null if the location permissions aren't granted or the location service is turned of.
+
+  LatLng? get rawLocation;
+
+  double get rawAccuracy;
+
+  /// Orientation may be null if the device doesn't provide the respective sensors.
+
+  double? get rawOrientation;
+
+
+  /// The time interval in which new location data should be fetched.
+  Duration get locationUpdateInterval;
+
+  /// The time interval in which new sensor data should be fetched.
+  Duration get orientationUpdateInterval;
+
+  /// The minimal distance difference in meters of the new and the previous position that will be detected as a change.
+  int get locationDifferenceThreshold;
+
+  /// The minimal difference in radians of the new and the previous orientation that will be detected as a change.
+  double get orientationDifferenceThreshold;
+
+
+  /// The minimal difference in meters of the new and the previous accuracy that will be detected as a change.
+  double get accuracyDifferenceThreshold;
+
+
+  bool get isActive;
+
+  void activate();
+
+  void deactivate();
+
+  /// Calls listener every time the raw location or orientation changes.
+  void addRawListener(VoidCallback listener);
+
+  /// Stops calling the listener every time the raw location or orientation changes.
+  void removeRawListener(VoidCallback listener);
 }
 
 /// Used to hide internal setters and functions.
 
 class AnimatedLocationControllerImpl extends ChangeNotifier implements AnimatedLocationController {
-  AnimatedLocationControllerImpl._();
+  StreamSubscription<Position>? _locationStreamSub;
+  StreamSubscription<SensorEvent>? _orientationStreamSub;
+
+  late final StreamSubscription<ServiceStatus> _serviceStatusStreamSub;
+
+  AnimatedLocationControllerImpl._({
+    Duration locationUpdateInterval = const Duration(milliseconds: 1000),
+    Duration orientationUpdateInterval = const Duration(milliseconds: 200),
+    int locationDifferenceThreshold = 1,
+    double orientationDifferenceThreshold = 0.1,
+    double accuracyDifferenceThreshold = 0.5,
+  }) :
+    _locationUpdateInterval = locationUpdateInterval,
+    _orientationUpdateInterval = orientationUpdateInterval,
+    _locationDifferenceThreshold = locationDifferenceThreshold,
+    _orientationDifferenceThreshold = orientationDifferenceThreshold,
+    _accuracyDifferenceThreshold = accuracyDifferenceThreshold
+  {
+    activate();
+
+    _serviceStatusStreamSub = Geolocator.getServiceStatusStream().listen((event) {
+      if (event == ServiceStatus.enabled) {
+        activate();
+      }
+      else {
+        deactivate();
+      }
+    });
+  }
+
+  @override
+  bool get isActive => _locationStreamSub != null;
+
+  @override
+  void activate() {
+    if (isActive) deactivate();
+    _setupLocationStream();
+    _setupRotationSensorStream();
+    notifyListeners();
+  }
+
+  @override
+  void deactivate() {
+    _cleanupLocationStream();
+    _cleanupRotationSensorStream();
+    notifyListeners();
+  }
+
+  Duration _locationUpdateInterval;
+  @override
+  Duration get locationUpdateInterval => _locationUpdateInterval;
+  set locationUpdateInterval(Duration value) {
+    if (value != _locationUpdateInterval) {
+      _locationUpdateInterval = value;
+      _cleanupLocationStream();
+      _setupLocationStream();
+    }
+  }
+
+  Duration _orientationUpdateInterval;
+  @override
+  Duration get orientationUpdateInterval => _orientationUpdateInterval;
+  set orientationUpdateInterval(Duration value) {
+    if (value != _orientationUpdateInterval) {
+      _orientationUpdateInterval = value;
+      _cleanupRotationSensorStream();
+      _setupRotationSensorStream();
+    }
+  }
+
+  int _locationDifferenceThreshold;
+  @override
+  int get locationDifferenceThreshold => _locationDifferenceThreshold;
+  set locationDifferenceThreshold(int value) {
+    if (value != _locationDifferenceThreshold) {
+      _locationDifferenceThreshold = value;
+      _cleanupLocationStream();
+      _setupLocationStream();
+    }
+  }
+
+  double _orientationDifferenceThreshold;
+  @override
+  double get orientationDifferenceThreshold => _orientationDifferenceThreshold;
+  set orientationDifferenceThreshold(double value) {
+    if (value != _orientationDifferenceThreshold) {
+      _orientationDifferenceThreshold = value;
+      _cleanupRotationSensorStream();
+      _setupRotationSensorStream();
+    }
+  }
+
+  double _accuracyDifferenceThreshold;
+  @override
+  double get accuracyDifferenceThreshold => _accuracyDifferenceThreshold;
+  set accuracyDifferenceThreshold(double value) {
+    if (value != _accuracyDifferenceThreshold) {
+      _accuracyDifferenceThreshold = value;
+      _cleanupLocationStream();
+      _setupLocationStream();
+    }
+  }
+
+  // animated/interpolated location and sensors value
 
   LatLng? _location;
-
   @override
   LatLng? get location => _location;
   set location(LatLng? value) {
@@ -35,7 +187,6 @@ class AnimatedLocationControllerImpl extends ChangeNotifier implements AnimatedL
   }
 
   double _accuracy = 0;
-
   @override
   double get accuracy => _accuracy;
   set accuracy(double value) {
@@ -46,7 +197,6 @@ class AnimatedLocationControllerImpl extends ChangeNotifier implements AnimatedL
   }
 
   double? _orientation;
-
   @override
   double? get orientation => _orientation;
   set orientation(double? value) {
@@ -54,5 +204,132 @@ class AnimatedLocationControllerImpl extends ChangeNotifier implements AnimatedL
       _orientation = value;
       notifyListeners();
     }
+  }
+
+  // raw location and sensor values
+
+  LatLng? _rawLocation;
+  @override
+  LatLng? get rawLocation => _rawLocation;
+
+  double _rawAccuracy = 0;
+  @override
+  double get rawAccuracy => _rawAccuracy;
+
+  double? _rawOrientation;
+  @override
+  double? get rawOrientation => _rawOrientation;
+
+  // raw listener methods
+
+  final _rawListeners = ObserverList<VoidCallback>();
+
+  @override
+  void addRawListener(VoidCallback listener) {
+    _rawListeners.add(listener);
+  }
+
+  @override
+  void removeRawListener(VoidCallback listener) {
+    _rawListeners.remove(listener);
+  }
+
+  void _notifyRawListeners() {
+    for (final listener in _rawListeners) {
+      listener();
+    }
+  }
+
+  // location methods
+
+  void _setupLocationStream() async {
+    final locationServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (locationServiceEnabled) {
+      final permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
+        _locationStreamSub = Geolocator.getPositionStream(
+          locationSettings: AndroidSettings(
+            intervalDuration: locationUpdateInterval,
+            distanceFilter: locationDifferenceThreshold,
+          )
+        ).listen(_handlePositionEvent, onError: (_) => deactivate());
+      }
+    }
+  }
+
+  void _cleanupLocationStream() {
+    _locationStreamSub?.cancel();
+    _locationStreamSub = null;
+  }
+
+  void _handlePositionEvent(Position event) {
+    _rawLocation = LatLng(event.latitude, event.longitude);
+
+    final newAccuracy = event.accuracy;
+    // check if difference threshold is reached
+    if ((_rawAccuracy - newAccuracy).abs() > accuracyDifferenceThreshold) {
+      _rawAccuracy = newAccuracy;
+    }
+    _notifyRawListeners();
+  }
+
+  // rotation sensor methods
+
+  void _setupRotationSensorStream() async {
+    if (await SensorManager().isSensorAvailable(Sensors.ROTATION)) {
+      final stream = await SensorManager().sensorUpdates(
+        sensorId: Sensors.ROTATION,
+        interval: orientationUpdateInterval,
+      );
+      _orientationStreamSub = stream.listen(_handleAbsoluteOrientationEvent);
+    }
+  }
+
+  void _cleanupRotationSensorStream() {
+    _orientationStreamSub?.cancel();
+    _orientationStreamSub = null;
+  }
+
+  void _handleAbsoluteOrientationEvent(SensorEvent event) {
+    const piDoubled = 2 * pi;
+    final double newOrientation;
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      // ios provides azimuth in degrees
+      newOrientation = degToRadian(event.data.first);
+    }
+    else if (defaultTargetPlatform == TargetPlatform.android) {
+      final g = event.data;
+      final norm = sqrt(g[0] * g[0] + g[1] * g[1] + g[2] * g[2] + g[3] * g[3]);
+      // normalize and set values to commonly known quaternion letter representatives
+      final x = g[0] / norm;
+      final y = g[1] / norm;
+      final z = g[2] / norm;
+      final w = g[3] / norm;
+      // calc azimuth in radians
+      final sinA = 2.0 * (w * z + x * y);
+      final cosA = 1.0 - 2.0 * (y * y + z * z);
+      final azimuth = atan2(sinA, cosA);
+      // convert from [-pi, pi] to [0,2pi]
+      newOrientation = (piDoubled - azimuth) % piDoubled;
+    }
+    else {
+      newOrientation = 0;
+    }
+
+    // check if difference threshold is reached
+    if (_rawOrientation == null ||
+      (_rawOrientation! - newOrientation).abs() > orientationDifferenceThreshold
+    ) {
+      _rawOrientation = newOrientation;
+      _notifyRawListeners();
+    }
+  }
+
+  @override
+  void dispose() {
+    _serviceStatusStreamSub.cancel();
+    _rawListeners.clear();
+    deactivate();
+    super.dispose();
   }
 }
--- lib/src/animated_location_layer.dart
+++ lib/src/animated_location_layer.dart
@@ -1,12 +1,8 @@
-import 'dart:async';
 import 'dart:math';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_sensors/flutter_sensors.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
 import '/src/animated_location_controller.dart';
@@ -23,32 +19,14 @@ enum CameraTrackingMode {
   none,
   /// The map camera will follow the user's location.
   location,
+  /// The map camera will rotate according the user's orientation.
+  orientation,
   /// The map camera will follow the user's location and rotate according the user's orientation.
   locationAndOrientation,
 }
 
 
 class AnimatedLocationLayer extends StatefulWidget {
-
-  /// The time interval in which new location data should be fetched.
-
-  final Duration locationUpdateInterval;
-
-  /// The time interval in which new sensor data should be fetched.
-
-  final Duration orientationUpdateInterval;
-
-  /// The minimal distance difference in meters of the new and the previous position that will be detected as a change.
-
-  final int locationDifferenceThreshold;
-
-  /// The minimal difference in radians of the new and the previous orientation that will be detected as a change.
-
-  final double orientationDifferenceThreshold;
-
-  /// The minimal difference in meters of the new and the previous accuracy that will be detected as a change.
-
-  final double accuracyDifferenceThreshold;
 
   /// A custom location indicator widget that replaces the default.
 
@@ -94,11 +72,6 @@ class AnimatedLocationLayer extends StatefulWidget {
   final AnimatedLocationController? controller;
 
   const AnimatedLocationLayer({
-    this.locationUpdateInterval = const Duration(milliseconds: 1000),
-    this.orientationUpdateInterval = const Duration(milliseconds: 200),
-    this.locationDifferenceThreshold = 1,
-    this.orientationDifferenceThreshold = 0.1,
-    this.accuracyDifferenceThreshold = 0.5,
     this.locationIndicator = const LocationIndicator(),
     this.accuracyIndicator = const AccuracyIndicator(),
     this.orientationIndicator = const OrientationIndicator(),
@@ -119,20 +92,6 @@ class AnimatedLocationLayer extends StatefulWidget {
 
 
 class _AnimatedLocationLayerState extends State<AnimatedLocationLayer> with SingleTickerProviderStateMixin {
-  static const _piDoubled = 2 * pi;
-
-  static const _earthCircumference = _piDoubled * earthRadius;
-
-  // TODO: Move interpolation and state entirely into the controller.
-  // So the animation/interpolation is not done by the widgets and instead by the controller.
-  // The widgets then simply consume these values.
-
-  LatLng? _location;
-
-  double _accuracy = 0;
-
-  double? _orientation;
-
   AnimatedLocationController? _internalController;
   AnimatedLocationControllerImpl get _controller => (
       widget.controller
@@ -142,30 +101,16 @@ class _AnimatedLocationLayerState extends State<AnimatedLocationLayer> with Sing
 
   MapCamera get _mapCamera => MapCamera.of(context);
 
-  StreamSubscription<Position>? _locationStreamSub;
-  StreamSubscription<SensorEvent>? _orientationStreamSub;
-
-  late final StreamSubscription<ServiceStatus> _serviceStatusStreamSub;
-
   @override
   void initState() {
     super.initState();
-    _setupSensorStreams();
-
-    _serviceStatusStreamSub = Geolocator.getServiceStatusStream().listen((event) {
-      _cleanupSensorStreams();
-      _setupSensorStreams();
-    });
-
+    _controller.addRawListener(_updateIndicator);
     _controller.addListener(_updateCamera);
   }
 
   @override
   void didUpdateWidget(covariant AnimatedLocationLayer oldWidget) {
     super.didUpdateWidget(oldWidget);
-    _cleanupSensorStreams();
-    _setupSensorStreams();
-
     if (widget.controller != oldWidget.controller) {
       (oldWidget.controller ?? _internalController)?.removeListener(_updateCamera);
     }
@@ -173,25 +118,25 @@ class _AnimatedLocationLayerState extends State<AnimatedLocationLayer> with Sing
 
   @override
   Widget build(BuildContext context) {
-    if (!_isVisible) return const SizedBox.shrink();
+    if (!_controller.isActive && !_isVisible) return const SizedBox.shrink();
 
     return MobileLayerTransformer(
       child: LocationIndicatorWrapper(
-        position: _location!,
+        position: _controller.rawLocation!,
         duration: widget.locationAnimationDuration,
         curve: widget.locationAnimationCurve,
         controller: _controller,
         children: [
-          if (_accuracy > 0) AccuracyIndicatorWrapper(
-            radius: _accuracy,
+          if (_controller.rawAccuracy > 0) AccuracyIndicatorWrapper(
+            radius: _controller.rawAccuracy,
             scale: _scale,
             duration: widget.accuracyAnimationDuration,
             curve: widget.accuracyAnimationCurve,
             controller: _controller,
             child: widget.accuracyIndicator,
           ),
-          if (_orientation != null) OrientationIndicatorWrapper(
-            orientation: _orientation!,
+          if (_controller.rawOrientation != null) OrientationIndicatorWrapper(
+            orientation: _controller.rawOrientation!,
             duration: widget.orientationAnimationDuration,
             curve: widget.orientationAnimationCurve,
             controller: _controller,
@@ -204,108 +149,24 @@ class _AnimatedLocationLayerState extends State<AnimatedLocationLayer> with Sing
   }
 
 
-  void _setupSensorStreams() async {
-    final locationServiceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (locationServiceEnabled) {
-      final permission = await Geolocator.checkPermission();
-      if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
-        _locationStreamSub = Geolocator.getPositionStream(
-          locationSettings: AndroidSettings(
-            intervalDuration: widget.locationUpdateInterval,
-            distanceFilter: widget.locationDifferenceThreshold,
-          )
-        ).listen(_handlePositionEvent, onError: (_) {});
-      }
-    }
-
-    if (await SensorManager().isSensorAvailable(Sensors.ROTATION)) {
-      final stream = await SensorManager().sensorUpdates(
-        sensorId: Sensors.ROTATION,
-        interval: widget.orientationUpdateInterval,
-      );
-      _orientationStreamSub = stream.listen(_handleAbsoluteOrientationEvent);
-    }
-  }
-
-
-  void _cleanupSensorStreams() {
-    _locationStreamSub?.cancel();
-    _orientationStreamSub?.cancel();
-    _locationStreamSub = null;
-    _orientationStreamSub = null;
-  }
-
-  double get _scale => _location != null
-    ? _calculateMetersPerPixel(_location!.latitude, _mapCamera.zoom) : 1;
+  double get _scale => _controller.rawLocation != null
+    ? _calculateMetersPerPixel(_controller.rawLocation!.latitude, _mapCamera.zoom) : 1;
 
 
   bool get _isVisible {
-    final location = _controller.location ?? _location;
+    final location = _controller.location ?? _controller.rawLocation;
     if (location == null) return false;
-    final accuracyInPixel = _accuracy / _scale;
+    final accuracyInPixel = _controller.accuracy / _scale;
     final biggestSize = max(accuracyInPixel, 100);
     final positionInPixel = _mapCamera.project(location);
     final sw = Point(positionInPixel.x + biggestSize, positionInPixel.y - biggestSize);
     final ne = Point(positionInPixel.x - biggestSize, positionInPixel.y + biggestSize);
-
     return _mapCamera.pixelBounds.containsPartialBounds(Bounds(sw, ne));
   }
 
-
-  void _handlePositionEvent(Position event) {
-    setState(() {
-      _location = LatLng(event.latitude, event.longitude);
-    });
-
-    final newAccuracy = event.accuracy;
-    // check if difference threshold is reached
-    if ((_accuracy - newAccuracy).abs() > widget.accuracyDifferenceThreshold) {
-      setState(() {
-        _accuracy = newAccuracy;
-      });
-    }
+  void _updateIndicator() {
+    setState(() {});
   }
-
-
-  void _handleAbsoluteOrientationEvent(SensorEvent event) {
-    final double newOrientation;
-
-    // don't rebuild widget when indicator is not visible in order to prevent repaints
-    if (_isVisible) {
-      if (defaultTargetPlatform == TargetPlatform.iOS) {
-        // ios provides azimuth in degrees
-        newOrientation = degToRadian(event.data.first);
-      }
-      else if (defaultTargetPlatform == TargetPlatform.android) {
-        final g = event.data;
-        final norm = sqrt(g[0] * g[0] + g[1] * g[1] + g[2] * g[2] + g[3] * g[3]);
-        // normalize and set values to commonly known quaternion letter representatives
-        final x = g[0] / norm;
-        final y = g[1] / norm;
-        final z = g[2] / norm;
-        final w = g[3] / norm;
-        // calc azimuth in radians
-        final sinA = 2.0 * (w * z + x * y);
-        final cosA = 1.0 - 2.0 * (y * y + z * z);
-        final azimuth = atan2(sinA, cosA);
-        // convert from [-pi, pi] to [0,2pi]
-        newOrientation = (_piDoubled - azimuth) % _piDoubled;
-      }
-      else {
-        newOrientation = 0;
-      }
-
-      // check if difference threshold is reached
-      if (_orientation == null ||
-        (_orientation! - newOrientation).abs() > widget.orientationDifferenceThreshold
-      ) {
-        setState(() {
-          _orientation = newOrientation;
-        });
-      }
-    }
-  }
-
 
   /// Handles camera position and rotation updates when the AnimatedLocationController changes
 
@@ -315,39 +176,52 @@ class _AnimatedLocationLayerState extends State<AnimatedLocationLayer> with Sing
       await SchedulerBinding.instance.endOfFrame;
     }
     if (!mounted) return;
+    final controller = MapController.of(context);
 
-    if (widget.cameraTrackingMode != CameraTrackingMode.none && _controller.location != null) {
-      final controller = MapController.of(context);
-      if (widget.cameraTrackingMode == CameraTrackingMode.locationAndOrientation && _controller.orientation != null) {
-        // (counter) rotate map so the map faces always the direction the user is facing
-        controller.moveAndRotate(
-          _controller.location!,
-          _mapCamera.zoom,
-          _controller.orientation! * (-180/pi),
-          id: 'AnimatedLocationLayerCameraTracking',
-        );
-      }
-      else {
-        controller.move(
-          _controller.location!,
-          _mapCamera.zoom,
-          id: 'AnimatedLocationLayerCameraTracking',
-        );
-      }
+    if (widget.cameraTrackingMode == CameraTrackingMode.locationAndOrientation &&
+        _controller.location != null &&
+        _controller.orientation != null
+    ) {
+      // (counter) rotate map so the map faces always the direction the user is facing
+      controller.moveAndRotate(
+        _controller.location!,
+        _mapCamera.zoom,
+        _controller.orientation! * (-180/pi),
+        id: 'AnimatedLocationLayerCameraTracking',
+      );
+    }
+    else if (widget.cameraTrackingMode == CameraTrackingMode.location &&
+              _controller.location != null
+    ) {
+      controller.move(
+        _controller.location!,
+        _mapCamera.zoom,
+        id: 'AnimatedLocationLayerCameraTracking',
+      );
+    }
+    else if (widget.cameraTrackingMode == CameraTrackingMode.orientation &&
+              _controller.orientation != null
+    ) {
+      // (counter) rotate map so the map faces always the direction the user is facing
+      controller.rotate(
+        _controller.orientation! * (-180/pi),
+        id: 'AnimatedLocationLayerCameraTracking',
+      );
     }
   }
 
-
   double _calculateMetersPerPixel(double latitude, double zoomLevel) {
+    const piDoubled = 2 * pi;
+    const earthCircumference = piDoubled * earthRadius;
+
     final latitudeRadians = latitude * (pi/180);
-    return _earthCircumference * cos(latitudeRadians) / pow(2, zoomLevel + 8);
+    return earthCircumference * cos(latitudeRadians) / pow(2, zoomLevel + 8);
   }
 
 
   @override
   void dispose() {
-    _cleanupSensorStreams();
-    _serviceStatusStreamSub.cancel();
+    _controller.removeRawListener(_updateIndicator);
     _controller.removeListener(_updateCamera);
     _internalController?.dispose();
     super.dispose();
